0a2dd559300ada974401d05ae780199109df680c
phimaster0303 <94635072+phimaster0303@users.noreply.github.com>
2024-06-19 22:22:27 +0200

    Update main.py
    made it better

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ import pytube
 import os
 
 try:
-    os.remove(r"C:\dloutput\output.mp4")
+    os.remove(r"output.mp4")
 except:
     None
 
 try:
-    os.remove(r"C:\dloutput\output.mp3")
+    os.remove(r"output.mp3")
 except:
     None
-
-print(r"The files will be named output.mp3 and output.mp4 and will be stored in C:\dloutput\ ")
+    
 def mp3(link):
     yt = pytube.YouTube(str(link))
     vid = yt.streams.get_highest_resolution()
     try:
-        vid.download(filename="output.mp4", output_path=r"C:\mpo")
-        os.system(rf"ffmpeg -i C:\dloutput\output.mp4 C:\mpo\output.mp3")
+        vid.download(filename="output.mp4", output_path="")
+        os.system(rf"ffmpeg -i output.mp4 output.mp3")
 
     except:
         print("error")
